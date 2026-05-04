469e69c3d584e86dbf0d88b6889a7606a20d4ba9
gimunkwon <gimon71@gmail.com>
2026-05-04 22:00:36 +0900

    [Add]  DT에 AIPerceptionComponent 값 형식 열에 추가

--- Source/Clamor_Libertatis/Public/Enemy/DataTable/DT_BaseEnemy.h
+++ Source/Clamor_Libertatis/Public/Enemy/DataTable/DT_BaseEnemy.h
@@ -12,4 +12,12 @@ struct FBaseEnemyStat : public FTableRowBase
 	float Enemy_HP = 0.f;
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="BaseEnemyStat")
 	float Enemy_AttackDamage = 0.f;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="BaseEnemyStat")
+	float Enemy_SightRadius = 0.f;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="BaseEnemyStat")
+	float Enemy_LoseSightRadius = 0.f;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="BaseEnemyStat")
+	float Enemy_PeripheralVisionAngleDegrees = 0.f;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="BaseEnemyStat")
+	float Enemy_SightConfig_MaxAge = 0.f;
 };
