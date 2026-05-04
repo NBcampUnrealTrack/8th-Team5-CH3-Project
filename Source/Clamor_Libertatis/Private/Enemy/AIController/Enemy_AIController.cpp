3c6e785d17d31d106babeac9349a32bed54dd0d4
gimunkwon <gimon71@gmail.com>
2026-05-04 19:00:03 +0900

    [Add] AIController AIPerception,SenseConfig 초기화및 감지 유무에따른 바인딩함수 구현

--- Source/Clamor_Libertatis/Private/Enemy/AIController/Enemy_AIController.cpp
+++ Source/Clamor_Libertatis/Private/Enemy/AIController/Enemy_AIController.cpp
@@ -1,10 +1,27 @@
 #include "Enemy/AIController/Enemy_AIController.h"
 
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
 
 
 AEnemy_AIController::AEnemy_AIController()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	
+	AIPerceptionComp = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerceptionComponent"));
+	Sight_Config = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight_Config"));
+	Sight_Config->SightRadius = 800.f;
+	Sight_Config->LoseSightRadius = 1000.f;
+	Sight_Config->PeripheralVisionAngleDegrees = 180.f;
+	Sight_Config->SetMaxAge(5.f);
+	
+	Sight_Config->DetectionByAffiliation.bDetectEnemies = true;
+	Sight_Config->DetectionByAffiliation.bDetectFriendlies = true;
+	Sight_Config->DetectionByAffiliation.bDetectNeutrals = true;
+	
+	AIPerceptionComp->ConfigureSense(*Sight_Config);
+	AIPerceptionComp->RequestStimuliListenerUpdate();
+	AIPerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this,&AEnemy_AIController::OnTargetPerceived);
 }
 
 
@@ -13,9 +30,28 @@ void AEnemy_AIController::BeginPlay()
 	Super::BeginPlay();
 }
 
-
 void AEnemy_AIController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	DrawDebugSphere(GetWorld(),GetPawn()->GetActorLocation(),Sight_Config->SightRadius,32,FColor::Green);
+	DrawDebugSphere(GetWorld(),GetPawn()->GetActorLocation(),Sight_Config->LoseSightRadius,32,FColor::Red);
 }
+
+
+void AEnemy_AIController::OnTargetPerceived(AActor* Actor, FAIStimulus Stimulus)
+{
+	if (Actor->ActorHasTag(TEXT("Player")))
+	{
+		if (Stimulus.WasSuccessfullySensed())
+		{
+			UE_LOG(LogTemp,Warning,TEXT("Player 감지 성공"));
+		}
+		else
+		{
+			UE_LOG(LogTemp,Warning,TEXT("Player 감지 실패"));
+		}
+	}
+}
+
+
 
--- Source/Clamor_Libertatis/Public/Enemy/AIController/Enemy_AIController.h
+++ Source/Clamor_Libertatis/Public/Enemy/AIController/Enemy_AIController.h
@@ -4,15 +4,25 @@
 #include "Runtime/AIModule/Classes/AIController.h"
 #include "Enemy_AIController.generated.h"
 
+struct FAIStimulus;
+class UAISenseConfig_Sight;
+
 UCLASS()
 class CLAMOR_LIBERTATIS_API AEnemy_AIController : public AAIController
 {
 	GENERATED_BODY()
-
 public:
 	AEnemy_AIController();
 protected:
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AIPerception")
+	TObjectPtr<UAIPerceptionComponent> AIPerceptionComp;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AIPerception")
+	TObjectPtr<UAISenseConfig_Sight> Sight_Config;
+	
+	UFUNCTION()
+	void OnTargetPerceived(AActor* Actor ,FAIStimulus Stimulus);
 public:
 	virtual void Tick(float DeltaTime) override;
 };
