8f9489a619d1984c73163613b17b7370f27d633c
gimunkwon <gimon71@gmail.com>
2026-05-04 17:13:17 +0900

    [Refac] 구조체 멤버변수 초기화

--- Source/Clamor_Libertatis/Public/Enemy/DataTable/DT_BaseEnemy.h
+++ Source/Clamor_Libertatis/Public/Enemy/DataTable/DT_BaseEnemy.h
@@ -9,7 +9,7 @@ struct FBaseEnemyStat : public FTableRowBase
 	GENERATED_BODY()
 	
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="BaseEnemyStat")
-	float Enemy_HP;
+	float Enemy_HP = 0.f;
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="BaseEnemyStat")
-	float Enemy_AttackDamage;
+	float Enemy_AttackDamage = 0.f;
 };
