35c8418dafb1aad18da63cb06e3c40c5c9bb4a2b
gimunkwon <gimon71@gmail.com>
2026-05-04 11:56:03 +0900

    [Add] Binary 구분용 테스트클래스 추가(삭제 예정)

---
+++ Source/Clamor_Libertatis/Private/Test/TestBinary_Remove.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Clamor_Libertatis/Public/Test/TestBinary_Remove.h"
+
+
+TestBinary_Remove::TestBinary_Remove()
+{
+}
+
+TestBinary_Remove::~TestBinary_Remove()
+{
+}
---
+++ Source/Clamor_Libertatis/Public/Test/TestBinary_Remove.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class CLAMOR_LIBERTATIS_API TestBinary_Remove
+{
+public:
+	TestBinary_Remove();
+	~TestBinary_Remove();
+};
