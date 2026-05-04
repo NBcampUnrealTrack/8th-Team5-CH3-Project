001895c19a582aec05d6f34c9ecacb4c6bf0f82f
gimunkwon <gimon71@gmail.com>
2026-05-04 16:20:43 +0900

    [Add] Enemy EquipWeapon 메서드 추가및 Beginplay에서 호출

--- Source/Clamor_Libertatis/Public/Enemy/BaseEnemy.h
+++ Source/Clamor_Libertatis/Public/Enemy/BaseEnemy.h
@@ -4,23 +4,24 @@
 #include "GameFramework/Character.h"
 #include "BaseEnemy.generated.h"
 
+class AEnemy_BaseWeapon;
+
 UCLASS()
 class CLAMOR_LIBERTATIS_API ABaseEnemy : public ACharacter
 {
 	GENERATED_BODY()
 
 public:
-	
 	ABaseEnemy();
-
 protected:
-	
 	virtual void BeginPlay() override;
-
+	
+	void EquipWeapon();
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Weapon")
+	TSubclassOf<AActor> Enemy_WeaponClass;
+	UPROPERTY()
+	TObjectPtr<AEnemy_BaseWeapon> Enemy_WeaponInst;
 public:
-	
 	virtual void Tick(float DeltaTime) override;
-
-	
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
