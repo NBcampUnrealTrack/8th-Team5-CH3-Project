7520242ddab0a00e7811fa4e14413cf7c0733a27
박지석 <qkrwltjr0331@gmail.com>
2026-05-04 21:07:08 +0900

    C++ 에서 AnimMontage사용을 위해 Build.cs 수정

--- Source/Clamor_Libertatis/Clamor_Libertatis.Build.cs
+++ Source/Clamor_Libertatis/Clamor_Libertatis.Build.cs
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 
@@ -8,7 +8,7 @@ public class Clamor_Libertatis : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AnimGraphRuntime" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
