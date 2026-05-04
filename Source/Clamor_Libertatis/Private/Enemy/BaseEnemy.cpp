600efe7f0295d88acc3fdbbcc9e4d02fab3ce409
gimunkwon <gimon71@gmail.com>
2026-05-04 16:20:43 +0900

    [Add] Enemy EquipWeapon 메서드 추가및 Beginplay에서 호출

--- Source/Clamor_Libertatis/Private/Enemy/BaseEnemy.cpp
+++ Source/Clamor_Libertatis/Private/Enemy/BaseEnemy.cpp
@@ -1,10 +1,10 @@
 #include "Enemy/BaseEnemy.h"
 
+#include "Enemy/E_Weapon/Enemy_BaseWeapon.h"
 
 
 ABaseEnemy::ABaseEnemy()
 {
-	
 	PrimaryActorTick.bCanEverTick = true;
 }
 
@@ -12,18 +12,25 @@ ABaseEnemy::ABaseEnemy()
 void ABaseEnemy::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	EquipWeapon();
 }
-
 
 void ABaseEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
 
-
-void ABaseEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+void ABaseEnemy::EquipWeapon()
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (Enemy_WeaponClass)
+	{
+		Enemy_WeaponInst = Cast<AEnemy_BaseWeapon>(GetWorld()->SpawnActor<AActor>(Enemy_WeaponClass,FVector::ZeroVector,FRotator::ZeroRotator));
+		if (Enemy_WeaponInst)
+		{
+			FAttachmentTransformRules AttachmentTransformRules(EAttachmentRule::SnapToTarget, true);
+		
+			Enemy_WeaponInst->AttachToComponent(GetMesh(),AttachmentTransformRules,TEXT("WeaponSocket"));
+			UE_LOG(LogTemp,Warning,TEXT("Enemy Weapon Initialized"));
+		}
+	}
 }
-
