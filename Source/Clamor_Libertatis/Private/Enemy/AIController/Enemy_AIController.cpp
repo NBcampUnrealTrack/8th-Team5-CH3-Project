f38c41a2954528b51f49aa6884a290aa47a3cd01
gimunkwon <gimon71@gmail.com>
2026-05-04 12:03:53 +0900

    [Add] AIController 클래스 추가 BaseEnemy 클래스 추가 AIModule 추가

---
+++ Source/Clamor_Libertatis/Private/Enemy/AIController/Enemy_AIController.cpp
@@ -0,0 +1,23 @@
+#include "Enemy/AIController/Enemy_AIController.h"
+
+
+
+AEnemy_AIController::AEnemy_AIController()
+{
+	
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+
+void AEnemy_AIController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void AEnemy_AIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
