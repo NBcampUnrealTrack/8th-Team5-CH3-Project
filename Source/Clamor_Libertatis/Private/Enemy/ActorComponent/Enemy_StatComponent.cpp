660d8fd1db2c5f75fa94d76468d293176c46e95c
gimunkwon <gimon71@gmail.com>
2026-05-04 17:13:44 +0900

    [Add] ActorComponent DT연동 스탯 초기화 메서드 추가

--- Source/Clamor_Libertatis/Private/Enemy/ActorComponent/Enemy_StatComponent.cpp
+++ Source/Clamor_Libertatis/Private/Enemy/ActorComponent/Enemy_StatComponent.cpp
@@ -1,5 +1,6 @@
 #include "Enemy/ActorComponent/Enemy_StatComponent.h"
 
+#include "Enemy/DataTable/DT_BaseEnemy.h"
 
 
 UEnemy_StatComponent::UEnemy_StatComponent()
@@ -7,14 +8,10 @@ UEnemy_StatComponent::UEnemy_StatComponent()
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
-
-
 void UEnemy_StatComponent::BeginPlay()
 {
 	Super::BeginPlay();
 }
-
-
 
 void UEnemy_StatComponent::TickComponent(float DeltaTime, ELevelTick TickType,
                                          FActorComponentTickFunction* ThisTickFunction)
@@ -22,3 +19,16 @@ void UEnemy_StatComponent::TickComponent(float DeltaTime, ELevelTick TickType,
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
 
+void UEnemy_StatComponent::InitializeEnemyStat()
+{
+	if (!DT_EnemyRowHandle.IsNull())
+	{
+		const static FString ContextString = "Initialized Stat by Row";
+		if (FBaseEnemyStat* EnemyStat = DT_EnemyRowHandle.GetRow<FBaseEnemyStat>(ContextString))
+		{
+			HP = EnemyStat->Enemy_HP;
+			Attack_Damage = EnemyStat->Enemy_AttackDamage;
+			UE_LOG(LogTemp,Warning,TEXT("Initialized Enemy Stat"));
+		}
+	}
+}
--- Source/Clamor_Libertatis/Public/Enemy/ActorComponent/Enemy_StatComponent.h
+++ Source/Clamor_Libertatis/Public/Enemy/ActorComponent/Enemy_StatComponent.h
@@ -12,8 +12,23 @@ class CLAMOR_LIBERTATIS_API UEnemy_StatComponent : public UActorComponent
 
 public:
 	UEnemy_StatComponent();
+	
+	void InitializeEnemyStat();
+	FORCEINLINE float GetEnemyHP() const {return HP;}
+	FORCEINLINE float GetEnemyAttack_Damage() const {return Attack_Damage;}
 protected:
 	virtual void BeginPlay() override;
+	
+#pragma region EnemyStat
+	float HP;
+	float Attack_Damage;
+#pragma endregion 
+#pragma region DataTable
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="DataTable")
+	FDataTableRowHandle DT_EnemyRowHandle;
+#pragma endregion 
+	
+	
 public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
