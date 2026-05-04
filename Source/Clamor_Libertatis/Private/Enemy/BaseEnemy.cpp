bc5b4dc3ce324331eafa0ce7710739330d029b80
gimunkwon <gimon71@gmail.com>
2026-05-04 17:15:11 +0900

    [Add] Enemy의 ActorComponent의 스탯 초기화 메서드를 EnemyClass의 스탯초기화 메서드에서 호출후 Beginplay에서 호출

--- Source/Clamor_Libertatis/Private/Enemy/BaseEnemy.cpp
+++ Source/Clamor_Libertatis/Private/Enemy/BaseEnemy.cpp
@@ -1,18 +1,23 @@
 #include "Enemy/BaseEnemy.h"
 
+#include "Enemy/ActorComponent/Enemy_StatComponent.h"
 #include "Enemy/E_Weapon/Enemy_BaseWeapon.h"
 
 
 ABaseEnemy::ABaseEnemy()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	
+	Enemy_StatComp = CreateDefaultSubobject<UEnemy_StatComponent>(TEXT("StatComponent"));
 }
 
 
 void ABaseEnemy::BeginPlay()
 {
 	Super::BeginPlay();
+	
 	EquipWeapon();
+	InitializedStat();
 }
 
 void ABaseEnemy::Tick(float DeltaTime)
@@ -32,5 +37,14 @@ void ABaseEnemy::EquipWeapon()
 			Enemy_WeaponInst->AttachToComponent(GetMesh(),AttachmentTransformRules,TEXT("WeaponSocket"));
 			UE_LOG(LogTemp,Warning,TEXT("Enemy Weapon Initialized"));
 		}
+	}
+}
+
+void ABaseEnemy::InitializedStat()
+{
+	if (Enemy_StatComp)
+	{
+		Enemy_StatComp->InitializeEnemyStat();
+		UE_LOG(LogTemp,Warning,TEXT("Enemy HP: %f, Att : %f"),Enemy_StatComp->GetEnemyHP(), Enemy_StatComp->GetEnemyAttack_Damage());
 	}
 }
