63e75c1d933d6d16688fa2bba0e7f7e0d1b8ed8c
gimunkwon <gimon71@gmail.com>
2026-05-06 17:16:23 +0900

    [Add] 현재 캐릭터의 속도 값을 미리 캐싱해서 사용하도록 변수로 선언

--- Source/Clamor_Libertatis/Private/Enemy/Animations/BaseEnemyAnimInst.cpp
+++ Source/Clamor_Libertatis/Private/Enemy/Animations/BaseEnemyAnimInst.cpp
@@ -1,1 +1,23 @@
 #include "Enemy/Animations/BaseEnemyAnimInst.h"
+
+#include "Enemy/BaseEnemy.h"
+
+void UBaseEnemyAnimInst::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	MyOwnerInst = Cast<ABaseEnemy>(TryGetPawnOwner());
+}
+
+void UBaseEnemyAnimInst::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	if (!MyOwnerInst)
+	{
+		MyOwnerInst = Cast<ABaseEnemy>(TryGetPawnOwner());
+	}
+	
+	FVector Velocity = MyOwnerInst->GetVelocity();
+	Enemy_GroundSpeed = Velocity.Size2D();
+}
--- Source/Clamor_Libertatis/Public/Enemy/Animations/BaseEnemyAnimInst.h
+++ Source/Clamor_Libertatis/Public/Enemy/Animations/BaseEnemyAnimInst.h
@@ -4,8 +4,19 @@
 #include "Animation/AnimInstance.h"
 #include "BaseEnemyAnimInst.generated.h"
 
+class ABaseEnemy;
+
 UCLASS()
 class CLAMOR_LIBERTATIS_API UBaseEnemyAnimInst : public UAnimInstance
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+protected:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Stat")
+	float Enemy_GroundSpeed;
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Instance")
+	TObjectPtr<ABaseEnemy> MyOwnerInst;
 };
