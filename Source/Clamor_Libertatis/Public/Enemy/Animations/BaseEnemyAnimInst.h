3848d06c7948551693dd3af1f3d82a3d720c7bd6
gimunkwon <gimon71@gmail.com>
2026-05-06 16:10:59 +0900

    [Add] 모든 적들의 기초 애님 인스턴스 클래스 추가

---
+++ Source/Clamor_Libertatis/Public/Enemy/Animations/BaseEnemyAnimInst.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BaseEnemyAnimInst.generated.h"
+
+UCLASS()
+class CLAMOR_LIBERTATIS_API UBaseEnemyAnimInst : public UAnimInstance
+{
+	GENERATED_BODY()
+};
