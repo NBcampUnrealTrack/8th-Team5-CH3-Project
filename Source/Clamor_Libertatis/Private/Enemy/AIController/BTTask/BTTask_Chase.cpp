1f003ffc408bafb5ba4977f21ca27e29400fd236
gimunkwon <gimon71@gmail.com>
2026-05-06 11:17:19 +0900

    [Add] 노드를 실행하기 위한 메서드 오버라이드및 생성자 노드 이름 초기화(근데 MoveTo노드가 있어서 일단 나중에 정밀한 추적로직이 필요할때 수정할 예정)

--- Source/Clamor_Libertatis/Private/Enemy/AIController/BTTask/BTTask_Chase.cpp
+++ Source/Clamor_Libertatis/Private/Enemy/AIController/BTTask/BTTask_Chase.cpp
@@ -1,1 +1,11 @@
 #include "Enemy/AIController/BTTask/BTTask_Chase.h"
+
+UBTTask_Chase::UBTTask_Chase()
+{
+	NodeName = TEXT("Chase_Player");
+}
+
+EBTNodeResult::Type UBTTask_Chase::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return EBTNodeResult::Failed;
+}
--- Source/Clamor_Libertatis/Public/Enemy/AIController/BTTask/BTTask_Chase.h
+++ Source/Clamor_Libertatis/Public/Enemy/AIController/BTTask/BTTask_Chase.h
@@ -8,4 +8,8 @@ UCLASS()
 class CLAMOR_LIBERTATIS_API UBTTask_Chase : public UBTTaskNode
 {
 	GENERATED_BODY()
+public:
+	UBTTask_Chase();
+protected:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 };
