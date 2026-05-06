93886832359f522654512355d7d0eb3097996b24
gimunkwon <gimon71@gmail.com>
2026-05-06 12:15:33 +0900

    [Add] 적 공격용 테스크 노드 추가

---
+++ Source/Clamor_Libertatis/Public/Enemy/AIController/BTTask/BTTask_Attack.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_Attack.generated.h"
+
+UCLASS()
+class CLAMOR_LIBERTATIS_API UBTTask_Attack : public UBTTaskNode
+{
+	GENERATED_BODY()
+};
