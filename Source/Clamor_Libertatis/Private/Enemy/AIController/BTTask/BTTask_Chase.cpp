022775f281f71d2c1e66cac031812e242f58693c
gimunkwon <gimon71@gmail.com>
2026-05-06 10:54:31 +0900

    [add] TaskNode Chase 클래스 추가

---
+++ Source/Clamor_Libertatis/Private/Enemy/AIController/BTTask/BTTask_Chase.cpp
@@ -0,0 +1,1 @@
+#include "Enemy/AIController/BTTask/BTTask_Chase.h"
---
+++ Source/Clamor_Libertatis/Public/Enemy/AIController/BTTask/BTTask_Chase.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_Chase.generated.h"
+
+UCLASS()
+class CLAMOR_LIBERTATIS_API UBTTask_Chase : public UBTTaskNode
+{
+	GENERATED_BODY()
+};
