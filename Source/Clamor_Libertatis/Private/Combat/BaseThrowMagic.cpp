4dd7cf27b6df4e20d28b9612062c2c9edae1a808
박지석 <qkrwltjr0331@gmail.com>
2026-05-07 19:56:08 +0900

    마법?투사체 액터 구현. 이전에 추가한 마법 에셋과 같이 사용됨

---
+++ Source/Clamor_Libertatis/Private/Combat/BaseThrowMagic.cpp
@@ -0,0 +1,185 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Combat/BaseThrowMagic.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Character/PlayerCharacter.h"
+#include "Combat/Weapon/WeaponBase.h"
+#include "NiagaraComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraSystem.h"
+#include "kismet/GameplayStatics.h"
+
+// Sets default values
+ABaseThrowMagic::ABaseThrowMagic()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphere"));
+    SetRootComponent(SphereComponent);
+
+    SphereComponent->InitSphereRadius(20.0f);
+    SphereComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    SphereComponent->SetCollisionObjectType(ECC_WorldDynamic);
+
+    SphereComponent->SetCollisionResponseToAllChannels(ECR_Block);
+    SphereComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Block);
+
+    SphereComponent->SetNotifyRigidBodyCollision(true);
+    SphereComponent->SetGenerateOverlapEvents(false);
+    SphereComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	NiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("ProjectileEffect"));
+	NiagaraComponent->SetupAttachment(SphereComponent);
+	NiagaraComponent->SetAutoActivate(true);
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
+	ProjectileMovementComponent->UpdatedComponent = SphereComponent;
+	
+	ProjectileMovementComponent->InitialSpeed = InitialSpeed;
+	ProjectileMovementComponent->MaxSpeed = MaxSpeed;
+	ProjectileMovementComponent->bRotationFollowsVelocity = true;
+	ProjectileMovementComponent->bShouldBounce = false;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+
+    InitialLifeSpan = LifeTime;
+}
+
+// Called when the game starts or when spawned
+void ABaseThrowMagic::BeginPlay()
+{
+	Super::BeginPlay();
+	SphereComponent->OnComponentHit.AddDynamic(this, &ABaseThrowMagic::OnProjectileHit);
+
+	ProjectileMovementComponent->InitialSpeed = InitialSpeed;
+	ProjectileMovementComponent->MaxSpeed = MaxSpeed;
+
+
+    //생성후 CollisionEnableDelay초동안 충돌X
+    GetWorldTimerManager().SetTimer(
+        CollisionEnableTimerHandle,
+        this,
+        &ABaseThrowMagic::EnableCollision,
+        CollisionEnableDelay,
+        false
+    );
+}
+
+void ABaseThrowMagic::OnProjectileHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (!OtherActor || OtherActor == this)
+	{
+		return;
+	}
+
+    const FVector ExplosionLocation = Hit.ImpactPoint;
+
+    if (MagicHitEffect)
+    {
+        UNiagaraComponent* NiagaraComp = UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+            GetWorld(),
+            MagicHitEffect,
+            ExplosionLocation,
+            Hit.ImpactNormal.Rotation(),
+            FVector(EffectScale)
+        );
+        if (NiagaraComp)
+        {
+            //크기 파라미터 즉시수정.
+            NiagaraComp->SetVariableFloat(TEXT("Scale_All"), EffectScale);
+        }
+    }
+
+    ApplyExplosionDamage(ExplosionLocation);
+	Destroy();
+}
+void ABaseThrowMagic::ApplyExplosionDamage(const FVector& ExplosionLocation)
+{
+    UWorld* World = GetWorld();
+    if (!World)
+    {
+        return;
+    }
+
+    TArray<AActor*> IgnoreActors;
+    IgnoreActors.Add(this);
+    APlayerCharacter* Player = Cast<APlayerCharacter>(GetOwner());
+    if (Player)
+    {
+        IgnoreActors.Add(Player);
+    }
+
+    if (GetInstigator())
+    {
+        IgnoreActors.Add(GetInstigator());
+    }
+
+    TArray<FHitResult> HitResults;
+
+    FCollisionShape CollisionShape;
+    CollisionShape.SetSphere(ExplosionRadius);
+
+    FCollisionQueryParams QueryParams;
+    QueryParams.AddIgnoredActors(IgnoreActors);
+    QueryParams.bTraceComplex = false;
+
+    const bool bHit = World->SweepMultiByChannel(
+        HitResults,
+        ExplosionLocation,
+        ExplosionLocation,
+        FQuat::Identity,
+        ECC_Pawn,//<-Pawn 만 체크하는거임
+        CollisionShape,
+        QueryParams
+    );
+    //디버그 그리기
+    DrawDebugSphere(
+        World,
+        ExplosionLocation,
+        ExplosionRadius,
+        32,
+        FColor::Red,
+        false,
+        1.0f
+    );
+
+    if (!bHit)
+    {
+        return;
+    }
+
+    TSet<AActor*> DamagedActors;
+
+    AController* InstigatorController = GetInstigatorController();
+
+    for (const FHitResult& HitResult : HitResults)
+    {
+        AActor* HitActor = HitResult.GetActor();
+
+        if (!HitActor)
+        {
+            continue;
+        }
+
+        if (DamagedActors.Contains(HitActor))
+        {
+            continue;
+        }
+
+        DamagedActors.Add(HitActor);
+
+        UGameplayStatics::ApplyDamage(
+            HitActor,
+            DamageAmount,
+            InstigatorController,
+            this,
+            UDamageType::StaticClass()
+        );
+    }
+}
+
+void ABaseThrowMagic::EnableCollision()
+{
+    SphereComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+}
