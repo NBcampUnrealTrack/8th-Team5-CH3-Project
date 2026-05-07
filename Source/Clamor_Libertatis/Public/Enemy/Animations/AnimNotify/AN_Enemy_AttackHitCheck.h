c780aa105e57737293d93fd92781cc4302cc0e91
gimunkwon <gimon71@gmail.com>
2026-05-07 19:53:49 +0900

    [Add] Enemy용 커스텀 노티파이 클래스 작성및 Notify 메서드 재정의

--- Source/Clamor_Libertatis/Public/Enemy/Animations/AnimNotify/AN_Enemy_AttackHitCheck.h
+++ Source/Clamor_Libertatis/Public/Enemy/Animations/AnimNotify/AN_Enemy_AttackHitCheck.h
@@ -8,4 +8,6 @@ UCLASS()
 class CLAMOR_LIBERTATIS_API UAN_Enemy_AttackHitCheck : public UAnimNotify
 {
 	GENERATED_BODY()
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
 };
