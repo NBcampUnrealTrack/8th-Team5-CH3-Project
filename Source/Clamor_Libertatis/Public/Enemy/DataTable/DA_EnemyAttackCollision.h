713dfee7e7dd154a328dc31335640cda7844f749
gimunkwon <gimon71@gmail.com>
2026-05-07 20:15:37 +0900

    [Add] 스킬,공격별 콜리전 모양및 사이즈를 정의하기 위한 데이터에셋 추가

---
+++ Source/Clamor_Libertatis/Public/Enemy/DataTable/DA_EnemyAttackCollision.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DA_EnemyAttackCollision.generated.h"
+
+UCLASS()
+class CLAMOR_LIBERTATIS_API UDA_EnemyAttackCollision : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+};
