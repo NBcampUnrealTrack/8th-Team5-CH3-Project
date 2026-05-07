12c30a7957dd8e7b54508c998f65b6f13f6ea9ce
gimunkwon <gimon71@gmail.com>
2026-05-07 20:37:03 +0900

    [Add] 콜리전 관리 데이터에셋 클래스 작성및 에셋 추가

--- Source/Clamor_Libertatis/Public/Enemy/DataTable/DA_EnemyAttackCollision.h
+++ Source/Clamor_Libertatis/Public/Enemy/DataTable/DA_EnemyAttackCollision.h
@@ -1,11 +1,38 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "DA_BaseEnemyAnim.h"
 #include "Engine/DataAsset.h"
 #include "DA_EnemyAttackCollision.generated.h"
+
+
+UENUM()
+enum class EAttackCollisionShape : uint8
+{
+	Box,
+	Sphere
+};
+
+USTRUCT(BlueprintType)
+struct FEnemyAttackCollision
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AttackCollision")
+	EAttackCollisionShape CollisionShape;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AttackCollision")
+	float Distance;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AttackCollision")
+	FVector CollisionSize;
+};
+
 
 UCLASS()
 class CLAMOR_LIBERTATIS_API UDA_EnemyAttackCollision : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AttackCollision")
+	TMap<EAnimMontage,FEnemyAttackCollision> AttackCollisionData;
 };
