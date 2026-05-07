5e10d8e25ec484fc603c56157a0bbe0999a23b11
gimunkwon <gimon71@gmail.com>
2026-05-07 16:23:23 +0900

    [Add] 이넘값으로 애님몽타주 value값을 얻을수있는 게터메서드 구현

--- Source/Clamor_Libertatis/Public/Enemy/ActorComponent/Enemy_CombatComponent.h
+++ Source/Clamor_Libertatis/Public/Enemy/ActorComponent/Enemy_CombatComponent.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Enemy/DataTable/DA_BaseEnemyAnim.h"
 #include "Enemy_CombatComponent.generated.h"
 
 
@@ -15,6 +16,9 @@ class CLAMOR_LIBERTATIS_API UEnemy_CombatComponent : public UActorComponent
 
 public:
 	UEnemy_CombatComponent();
+	
+	FORCEINLINE int64 GetCurrentAMCount() const {return DA_EnemyAnim->Map_AM_Battle.Num();}
+	FORCEINLINE UAnimMontage* GetAnimMontage(EAnimMontage AnimMontage) const {return DA_EnemyAnim->Map_AM_Battle.FindRef(AnimMontage);}
 	
 	UPROPERTY()
 	TObjectPtr<UDA_BaseEnemyAnim> DA_EnemyAnim;
