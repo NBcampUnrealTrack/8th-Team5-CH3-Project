d4a7dbb58e17c326f2d13c6c175521aadb85e736
SUKMANBOK\rhgmd <sumannii036@gmail.com>
2026-05-07 19:30:26 +0900

    UI 테스트 맵 수정 및 데미지 텍스트 위젯 코드 수정

--- Source/Clamor_Libertatis/Private/UI/DamageTextWidget.cpp
+++ Source/Clamor_Libertatis/Private/UI/DamageTextWidget.cpp
@@ -10,6 +10,21 @@ void UDamageTextWidget::InitDamageText(float Damage)
 {
 	if (TB_Damage)
 	{
-		TB_Damage->SetText(FText::AsNumber((int32)Damage));
+		TB_Damage->SetText(
+			FText::AsNumber((int32)Damage)
+		);
+
+		FSlateFontInfo FontInfo =
+			TB_Damage->GetFont();
+
+		float FontSize = FMath::Clamp(
+			20.f + FMath::Sqrt(Damage) * 3.f,
+			20.f,
+			80.f
+		);
+
+		FontInfo.Size = FontSize;
+
+		TB_Damage->SetFont(FontInfo);
 	}
 }
