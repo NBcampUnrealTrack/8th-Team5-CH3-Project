c75de29632e24f7ba899f74f1aae3aee0435b31e
박지석 <qkrwltjr0331@gmail.com>
2026-05-10 18:21:09 +0900

    스킬 관련 코드파일 추가

---
+++ Source/Clamor_Libertatis/Public/Combat/Data/DA_SkillData.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "DA_SkillData.generated.h"
+
+
+UENUM(BlueprintType)
+enum class ESkillType : uint8
+{
+    Melee,
+    Projectile,
+    Area,
+    Buff,
+    Summon
+};
+
+UCLASS()
+class CLAMOR_LIBERTATIS_API UDA_SkillData : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+    
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FName SkillName;
+    
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FName Description;
+    
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    ESkillType SkillType;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    float StaminaCost = 0.f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    float ManaCost = 0.f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    float Cooldown = 0.f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    float Damage = 0.f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    float SkillRange = 0.f;
+    
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	float SphereRadius = 0.f;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TObjectPtr<UAnimMontage> CastMontage; //시전동작
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TSubclassOf<AActor> ProjectileClass;
+};
