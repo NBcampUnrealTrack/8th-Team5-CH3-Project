81596b607ec329999182ddcca3185b0ba8304fec
gimunkwon <gimon71@gmail.com>
2026-05-11 12:11:29 +0900

    [Refac] 기존 AttackCollision을 따로 DA로 가지고 있던것을 하나의 DA로 통합을해서 공격 타입에 따라 다른 배열에 구조체 정보를 가지고 있도록 수정

--- Source/Clamor_Libertatis/Public/Enemy/DataTable/DA_BaseEnemySkill.h
+++ Source/Clamor_Libertatis/Public/Enemy/DataTable/DA_BaseEnemySkill.h
@@ -2,28 +2,65 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
-#include "DA_BaseEnemyAnim.generated.h"
+#include "DA_BaseEnemySkill.generated.h"
 
 UENUM()
-enum class EAnimMontage : uint8
+enum class EAttackType : uint8
 {
-	AM_SweepAttack,
-	AM_JumpAttack,
-	AM_SingleAttack,
-	AM_3ComboAttack
+	NONE,
+	Attack_Normal,
+	Attack_Skill,
+	Attack_Skill_Phase
+};
+
+UENUM()
+enum class EAttackCollisionShape : uint8
+{
+	Box,
+	Sphere
+};
+
+USTRUCT(BlueprintType)
+struct FEnemyAttackCollision
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AttackCollision")
+	EAttackCollisionShape CollisionShape;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AttackCollision")
+	float Distance = 0.f;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AttackCollision")
+	FVector CollisionSize = FVector::ZeroVector;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AttackCollision")
+	float SphereRadius = 0.f;
+};
+
+USTRUCT(BlueprintType)
+struct FEnemySkillInfo
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AttackType")
+	EAttackType AttackType = EAttackType::NONE;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Animations|AnimMontage")
+	TObjectPtr<UAnimMontage> AM_TypeMontage = nullptr;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="Collision|Attack")
+	FEnemyAttackCollision AttackCollision;
 };
 
 
-
-
 UCLASS()
-class CLAMOR_LIBERTATIS_API UDA_BaseEnemyAnim : public UPrimaryDataAsset
+class CLAMOR_LIBERTATIS_API UDA_BaseEnemySkill : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
 	
 public:
-	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AnimMontage|Attack")
-	TMap<EAnimMontage,TObjectPtr<UAnimMontage>> Map_AM_Battle;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="EnemySkillInfo")
+	TArray<FEnemySkillInfo> Array_NormalAttack;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="EnemySkillInfo")
+	TArray<FEnemySkillInfo> Array_Skill_Attack;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="EnemySkillInfo")
+	TArray<FEnemySkillInfo> Array_Skill_PhaseAttack;
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category="AnimMontage|Dead")
 	TObjectPtr<UAnimMontage> AM_Dead;
 };
