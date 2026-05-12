035bfc7ad7dc481e579940e55155d6972cb62840
박지석 <qkrwltjr0331@gmail.com>
2026-05-12 17:08:22 +0900

    검 궤적 이펙트 임시적용 및 관련 부분 수정

---
+++ Source/Clamor_Libertatis/Public/Combat/Anim/WeaponTrail_TimedNiagara.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AnimNotifyState_TimedNiagaraEffect.h"
+#include "WeaponTrail_TimedNiagara.generated.h"
+
+class UNiagaraComponent;
+class UNiagaraSystem;
+class USkeletalMeshComponent;
+class UAnimSequenceBase;
+class UFXSystemComponent;
+
+UCLASS()
+class CLAMOR_LIBERTATIS_API UWeaponTrail_TimedNiagara : public UAnimNotifyState_TimedNiagaraEffect
+{
+	GENERATED_BODY()
+public:
+	UWeaponTrail_TimedNiagara();
+
+protected:
+	virtual UFXSystemComponent* SpawnEffect(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) const override;
+
+private:
+	UNiagaraSystem* GetWeaponTrailNiagara(USkeletalMeshComponent* MeshComp) const;
+};
