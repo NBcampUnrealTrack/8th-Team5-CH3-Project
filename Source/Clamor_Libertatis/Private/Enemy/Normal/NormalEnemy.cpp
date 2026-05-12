15e689f5fefae950da5fd5b2076680c11730acdb
gimunkwon <gimon71@gmail.com>
2026-05-12 11:11:13 +0900

    [Add] Hit 애님몽타주 재생이 끝나면 다시 BT 태스크 MoveTo가 동작할수 있도록 흐름제어

--- Source/Clamor_Libertatis/Private/Enemy/Normal/NormalEnemy.cpp
+++ Source/Clamor_Libertatis/Private/Enemy/Normal/NormalEnemy.cpp
@@ -1,5 +1,7 @@
 #include "Enemy/Normal/NormalEnemy.h"
 
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
 #include "Enemy/ActorComponent/Enemy_CombatComponent.h"
 #include "Enemy/Animations/BaseEnemyAnimInst.h"
 
@@ -39,11 +41,34 @@ void ANormalEnemy::HitReaction()
 {
 	UE_LOG(LogTemp,Warning,TEXT("HitReaction"));
 	
+	if (AAIController* AIC = Cast<AAIController>(GetController()))
+	{
+		if (AIC->GetBlackboardComponent())
+		{
+			AIC->GetBlackboardComponent()->SetValueAsBool(TEXT("bCanMove"),false);
+		}
+	}
+	
 	if (AnimInst)
 	{
 		if (UAnimMontage* AM_Hit = Enemy_CombatComp->DA_EnemySkill->AM_HitReaction)
 		{
+			FOnMontageEnded EndHitMontage;
+			EndHitMontage.BindUObject(this, &ANormalEnemy::OnHitMontageEnded);
 			AnimInst->Montage_Play(AM_Hit);
+			AnimInst->Montage_SetEndDelegate(EndHitMontage, AM_Hit);
+		}
+	}
+}
+
+void ANormalEnemy::OnHitMontageEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	if (AAIController* AIC = Cast<AAIController>(GetController()))
+	{
+		if (UBlackboardComponent* BB = AIC->GetBlackboardComponent())
+		{
+			BB->SetValueAsBool(TEXT("bCanMove"), true);
+			UE_LOG(LogTemp,Warning,TEXT("bCanMove true"));
 		}
 	}
 }
