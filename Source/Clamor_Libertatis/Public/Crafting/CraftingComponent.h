9acc886210955dea136be333ac67693bde5a5d27
JoYoungRae <godyoung36@gmail.com>
2026-05-12 17:54:48 +0900

    Crafting 기초

---
+++ Source/Clamor_Libertatis/Public/Crafting/CraftingComponent.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "CraftingComponent.generated.h"
+
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnCraftingFinished, bool, bSuccess);
+
+//class UItemData;
+class URecipeData;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class UCraftingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UCraftingComponent();
+
+	UFUNCTION(BlueprintCallable, Category = "Crafting")
+	void RequestCraft(URecipeData* Recipe);
+
+	UFUNCTION(BlueprintPure, Category = "Crafting")
+	bool CanCraft(URecipeData* Recipe) const;
+		
+	UPROPERTY(BlueprintAssignable, Category = "Crafting")
+	FOnCraftingFinished OnCraftingFinished;
+
+protected:
+	void ExecuteCraft(URecipeData* Recipe);
+};
---
+++ Source/Clamor_Libertatis/Public/Crafting/RecipeData.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "RecipeData.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FItemQuantity
+{
+	GENERATED_BODY()
+
+	// UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	// UItemData* ItemAsset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Amount;
+};
+
+
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class URecipeData : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// 필요 재료 목록
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Crafting")
+	TArray<FItemQuantity> Ingredients;
+
+	// 결과물 아이템
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Crafting")
+	FItemQuantity ResultItem;
+};
