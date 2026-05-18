83f614a69e54234bf2a0ad7d196636939e89b5d7
gimunkwon <gimon71@gmail.com>
2026-05-18 20:11:09 +0900

    [Delete] 기존 노티파이 틱에서 사운드재생하던 로직 삭제

--- Source/Clamor_Libertatis/Private/Enemy/Animations/AnimNotify/AN_Enemy_AttackHitCheck.cpp
+++ Source/Clamor_Libertatis/Private/Enemy/Animations/AnimNotify/AN_Enemy_AttackHitCheck.cpp
@@ -1,15 +1,15 @@
 #include "Enemy/Animations/AnimNotify/AN_Enemy_AttackHitCheck.h"
 
 #include "Enemy/BaseEnemy.h"
+#include "Kismet/GameplayStatics.h"
 
 void UAN_Enemy_AttackHitCheck::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
                                       const FAnimNotifyEventReference& EventReference)
 {
 	Super::Notify(MeshComp, Animation, EventReference);
-	
+
 	if (ABaseEnemy* MyOwner = Cast<ABaseEnemy>(MeshComp->GetOwner()))
 	{
 		MyOwner->AttackHitCheck();
 	}
-	
 }
