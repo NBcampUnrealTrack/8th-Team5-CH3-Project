7f9b912838b515d52c952d298c53dce48446680e
gimunkwon <gimon71@gmail.com>
2026-05-19 11:03:53 +0900

    [Add] BossEnemy의 드랍테이블을 관리할 액터컴포넌트 추가

---
+++ Source/Clamor_Libertatis/Private/Enemy/ActorComponent/Enemy_ItemDropTableComponent.cpp
@@ -0,0 +1,24 @@
+#include "Enemy/ActorComponent/Enemy_ItemDropTableComponent.h"
+
+
+
+UEnemy_ItemDropTableComponent::UEnemy_ItemDropTableComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+
+void UEnemy_ItemDropTableComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+
+void UEnemy_ItemDropTableComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                  FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
