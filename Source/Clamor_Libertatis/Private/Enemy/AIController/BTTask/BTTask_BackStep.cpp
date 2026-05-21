e9b2a0b48b9e00edf51162b77d0336ca084a2ebd
gimunkwon <gimon71@gmail.com>
2026-05-21 16:06:19 +0900

    [Fix] AddmovementInput 인자값 리플렉션 등록

--- Source/Clamor_Libertatis/Private/Enemy/AIController/BTTask/BTTask_BackStep.cpp
+++ Source/Clamor_Libertatis/Private/Enemy/AIController/BTTask/BTTask_BackStep.cpp
@@ -59,7 +59,7 @@ void UBTTask_BackStep::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMe
 	// 플레이어 방향 기준 좌/우 스트레이프
 	FVector ToPlayer = (TargetActor->GetActorLocation() - OwnerPawn->GetActorLocation()).GetSafeNormal2D();
 	FVector StrafeDir = FVector::CrossProduct(FVector::UpVector, ToPlayer) * Memory->StepDirection;
-	OwnerPawn->AddMovementInput(StrafeDir, 1.f);
+	OwnerPawn->AddMovementInput(StrafeDir, ScaleValue);
 
 	Memory->ElapsedTime += DeltaSeconds;
 	if (Memory->ElapsedTime >= SideStepDuration)
